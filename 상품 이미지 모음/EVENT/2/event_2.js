d67057e389f55a515ab5958b890e9400d1fcaad5
jeongsin25 <jeongsin25@naver.com>
2021-03-24 06:09:06 +0900

    update_4

--- 상품 이미지 모음/EVENT/2/event_2.js
+++ 상품 이미지 모음/EVENT/2/event_2.js
@@ -1,12 +1,13 @@
-// 장바구니 클릭
-var product_user_keep = document.getElementsByClassName('product_user_keep')[0];
-product_user_keep.addEventListener('click' , function(){
-    alert('장바구니에 담겼습니다.');
-})
-// 구매하기 클릭
-var product_user_buy = document.getElementsByClassName('product_user_buy')[0];
-product_user_buy.addEventListener('click' , function(){
-    if(confirm('구매하시겠습니까?')){
-        alert('공식 브랜드 홈페이지 가셔서 구매 부탁드립니다.');
+var product_ul = document.getElementsByClassName('product_ul')[0];
+product_ul.addEventListener('click' , function(event){
+    // 장바구니 클릭
+    if(event.target.classList.contains('product_user_keep')){
+        alert('장바구니에 담겼습니다.');
+    }
+    // 구매하기 클릭
+    else if(event.target.classList.contains('product_user_buy')){
+        if(confirm('구매하시겠습니까?')){
+            alert('공식 브랜드 홈페이지 가셔서 구매 부탁드립니다.');
+        }
     }
 })
